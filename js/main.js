eb6770c659388287848b10b44a563e2428d5cd33
ca1tlin22 <gagn0372@algonquinlive.com>
2016-03-15 12:41:49 -0400

    removed greater than/less than from ul, list appears

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 grains.forEach(function (item) {
-  var $ul = $('<ul>');
+  var $ul = $('ul');
   var $li = $('<li>');
-  var $h2 = $('<h2>');
   var $img = $('<img>');
   var $p = $('<p>');
+  var $h2 = $('<h2>');
 
   $ul.append($li);
   $img.attr('src', 'images/' + item.img);
