4f6c3553764b58c27ec0445e8b686229b7641a25
ca1tlin22 <gagn0372@algonquinlive.com>
2016-03-15 12:38:34 -0400

    items targeted and appended to appropriate object

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 grains.forEach(function (item) {
   var $ul = $('<ul>');
   var $newLi = $('<li>');
+  var $h2 = $('<h2>');
   var $img = $('<img>');
   var $p = $('<p>');
 
@@ -9,4 +10,8 @@ grains.forEach(function (item) {
 
   $img.attr('src', 'images/' + grains.img);
   $li.append($img);
+  $h2.html(item.name);
+  $li.append($h2);
+  $p.html(item.desc);
+  $li.append($p);
 });
