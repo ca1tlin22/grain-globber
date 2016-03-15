67237f6c898f6abf5d71dbb2879d5af0e3240445
ca1tlin22 <gagn0372@algonquinlive.com>
2016-03-15 12:40:00 -0400

    using li vs newLi

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 grains.forEach(function (item) {
   var $ul = $('<ul>');
-  var $newLi = $('<li>');
+  var $li = $('<li>');
   var $h2 = $('<h2>');
   var $img = $('<img>');
   var $p = $('<p>');
 
-  $newLi.html(grain.name);
-  $('ul').append($newLi);
-
-  $img.attr('src', 'images/' + grains.img);
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
   $li.append($img);
   $h2.html(item.name);
   $li.append($h2);
